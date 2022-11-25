663f4bb06c06dc0c13b783d3cc981fa719148b6c
YoshuaAditya <yoshuaadityakurnia@gmail.com>
2022-11-25 15:26:01 +0700

    Update Request6Progress

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,20 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+    //Deferred<T> is similar to Promise/Future, it will return T
+    val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
+        GlobalScope.async {   // #2
+            log("starting loading for ${repo.name}")
+            delay(3000)
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }//later specify Dispatchers.Main for updateUI function
+    }
+    return deferreds.awaitAll().flatten().aggregate()//flatten makes List<List<T>> into combined/flatten List<T>
 }
--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -1,11 +1,25 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.Dispatchers
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+    var allUsers = emptyList<User>()
+    for ((index, repo) in repos.withIndex()) {
+        val users = service.getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        allUsers = (allUsers + users).aggregate()
+        //There are 82 repos at the time writing, after each repo reach lastIndex, updateResults will run with data allUsers at that exact time
+        updateResults(allUsers, index == repos.lastIndex)
+    }
 }
