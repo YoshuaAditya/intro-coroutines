46c4b9f3bd3184569c30f6ef24bda3c0cc59fdf3
YoshuaAditya <yoshuaadityakurnia@gmail.com>
2022-11-25 15:01:26 +0700

    Update Request5Concurrent

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,21 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+    //Deferred<T> is similar to Promise/Future, it will return T
+    val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
+        // to run coroutines on different threads from the common thread pool, specify Dispatchers.Default as the context
+        async (Dispatchers.Default){
+            log("starting loading for ${repo.name}")
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }//later specify Dispatchers.Main for updateUI function
+    }
+    //you can also remove the return@coroutineScope, but i feel this makes more sense
+    return@coroutineScope deferreds.awaitAll().flatten().aggregate()//flatten makes List<List<T>> into combined/flatten List<T>
 }
