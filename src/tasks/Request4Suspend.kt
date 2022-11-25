6f6fd18cbe2195c4e5914592734684e1e0097f80
YoshuaAditya <yoshuaadityakurnia@gmail.com>
2022-11-25 09:14:32 +0700

    Update Request4

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -3,5 +3,16 @@ package tasks
 import contributors.*
 
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+    //previous call methods returns Call<>, Call.execute to load users.
+    //with the new method, it returns Response<>, Response automatically executes so we dont need .execute
+    return repos.flatMap { repo ->
+        service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+    }.aggregate()
 }
